b5903c6b78bc4ef9d4428ad80927b1784dc6d638
mikhail-7975 <mikhail.kalinichenko.1999@gmail.com>
2020-11-20 05:34:53 +0700

    simulate data reading

--- mainWindowController.py
+++ mainWindowController.py
@@ -2,19 +2,43 @@ from mainWindow import Ui_MainWindow
 from PyQt5 import QtWidgets, QtCore
 
 import random
+import math
+
+class CytometrCore:
+    def __init__(self):
+        self.triggerPort = None
+        self.tracer1Port = None
+        self.tracer2Port = None
+
+        self.triggerData = []
+        self.tracer1Data = []
+        self.tracer2Data = []
+
+        self.status = 0
+
+    def readData(self):
+        return [200 * math.exp(-((x - 100) / 10)**2 / 2) + random.randint(-10, 10) for x in range(0, 2000)], \
+               [200 * math.exp(-((x - 700) / 70)**2 / 2) + random.randint(-10, 10) for x in range(0, 2000)],\
+               [200 * math.exp(-((x - 1400) / 140)**2 / 2) + random.randint(-10, 10) for x in range(0, 2000)]
+
+CytoCore = CytometrCore()
 
 class mainWindowController(QtWidgets.QDialog, Ui_MainWindow):
     def __init__(self, parent = None):
         super(mainWindowController, self).__init__(parent)
         self.setupUi(self)
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.drawPlot)
 
         self.connectTracer1_pushButton.clicked.connect(self.connectTracer1_pushButton_clicked)
         self.startStop_button.clicked.connect(self.startStop_button_clicked)
 
     def drawPlot(self):
         self.graphic.clear()
-        self.graphic.plot([i for i in range(100)], [random.randint(-5, 5) for i in range(100)], pen ='g')
-        self.graphic.plot([i for i in range(100)], [random.randint(-5, 5) for i in range(100)], pen ='b')
+        trig, tr1, tr2 = CytoCore.readData()
+        self.graphic.plot([i for i in range(2000)], trig, pen ='r')
+        self.graphic.plot([i for i in range(2000)], tr1, pen ='g')
+        self.graphic.plot([i for i in range(2000)], tr2, pen ='b')
 
     @QtCore.pyqtSlot()
     def connectTracer1_pushButton_clicked(self):
@@ -22,5 +46,10 @@ class mainWindowController(QtWidgets.QDialog, Ui_MainWindow):
 
     @QtCore.pyqtSlot()
     def startStop_button_clicked(self):
-        self.drawPlot()
+        if CytoCore.status == 0:
+            self.timer.start(100)
+            CytoCore.status = 1
+        else:
+            self.timer.stop()
+            CytoCore.status = 0
         return 0
